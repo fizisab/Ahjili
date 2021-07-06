7c3f0537bd4d32731bdbdbb4e9b72b6ff9b1a7c2
fizisab <85132725+fizisab@users.noreply.github.com>
2021-07-06 12:43:29 +0500

    Add files via upload

---
+++ sys/config.php
@@ -0,0 +1,26 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://pixelphotoscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: pixelphotoscript@gmail.com   
+// +------------------------------------------------------------------------+
+// | Pixel Photo Script
+// | Copyright (c) 2018 pixelphoto. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "aljila";
+
+// Site URL
+$site_url = "http://localhost/aljila"; // e.g (http://example.com) 
+
+// Purchase code
+$purchase_code = "45f170c5-258a-436b-a8f1-7387e7602d1f"; // Your purchase code, don't give it to anyone. 
+$buyer = "alstorash"; // Your buyer name from envato. 
+?>
